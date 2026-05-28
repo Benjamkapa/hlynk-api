34d15c8c0c46df2fa96a2490c7aabd4e236ee72c
Benjamkapa <mkapabenjamin254@gmail.com>
2026-05-28 09:32:19 +0300

    Investigating a server looped server start

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'hlynk-server',
     script: 'index.js',
-    cwd: '/var/www/html/hlynk/actions-runner/hlynk/hlynk-api/hlynk-api/server',
+    cwd: './',
     interpreter: 'node',
     env: {
       NODE_ENV: 'production'
